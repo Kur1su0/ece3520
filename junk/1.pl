046f76ae35f148177d893776140b5143364ee8af
Kur1su0 <1997weizice@gmail.com>
2019-05-01 14:20:43 -0400

    update

--- junk/1.pl
+++
@@ -1,9 +0,0 @@
-:- set_pro_log_flag(verbose,silent).
-:- initialization main.
-
-main :-
-     ['which_shape.pro'].
-     sqA(["u","u"."r","r","d","d","l","l"]).
-     halt.
-main :-
-    halt(1).
--- junk/test.pl
+++
@@ -1,6 +0,0 @@
-:- initialization(main).
-
-main :-
-    ['/users/zicew/git/ece3520/sde1/which_shape.pro'].
-    sqA(["u"],[]).
-    halt(0).
